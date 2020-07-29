851a70178274727143942f587f7062de35a496ca
Guilherme Revolta <guilhermerevolta@gmail.com>
2020-07-28 21:30:13 -0300

    feat: Adding basic config

---
+++ api/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'gobarber_dev',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  },
+};
