eb2c004cbe812a4b6a9e2226a19e96c6939fa34f
Guilherme Revolta <guilhermerevolta@gmail.com>
2020-08-05 07:12:04 -0300

    feat: configuring nodemailer with mailtrap.io

---
+++ api/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "81940ee4a08df1",
+    pass: "951a8641e0f39f"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+}
